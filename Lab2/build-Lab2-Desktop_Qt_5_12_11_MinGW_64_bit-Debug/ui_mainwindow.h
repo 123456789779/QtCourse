8476db18a0bb40eee9d7587a6e6153fb490b0762
tang <2362087759@qq.com>
2025-11-07 10:34:57 +0800

    完成Lab2的ui部分

---
+++ Lab2/build-Lab2-Desktop_Qt_5_12_11_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,328 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.11
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionNew;
+    QAction *actionOpen;
+    QAction *actionSave;
+    QAction *actionSaveAs;
+    QAction *actionExit;
+    QAction *actionUndo;
+    QAction *actionCut;
+    QAction *actionCopy;
+    QAction *actionPaste;
+    QAction *actionFind;
+    QAction *actionReplace;
+    QAction *actionFont;
+    QAction *actionAutoWrap;
+    QAction *actionToolBar;
+    QAction *actionStatusBar;
+    QAction *actionRedo;
+    QAction *actionSelectAll;
+    QAction *actionLineNumber;
+    QAction *actionFontColor;
+    QAction *actionFontBackgourndColor;
+    QAction *actionEditorBackgroundColor;
+    QAction *actionAbout;
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QPlainTextEdit *textEdit;
+    QMenuBar *menubar;
+    QMenu *menu;
+    QMenu *menu_E;
+    QMenu *menu_O;
+    QMenu *menu_V;
+    QMenu *menu_H;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        actionNew = new QAction(MainWindow);
+        actionNew->setObjectName(QString::fromUtf8("actionNew"));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/img/new.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionNew->setIcon(icon);
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName(QString::fromUtf8("actionOpen"));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/img/open.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionOpen->setIcon(icon1);
+        actionSave = new QAction(MainWindow);
+        actionSave->setObjectName(QString::fromUtf8("actionSave"));
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/img/save.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionSave->setIcon(icon2);
+        actionSaveAs = new QAction(MainWindow);
+        actionSaveAs->setObjectName(QString::fromUtf8("actionSaveAs"));
+        QIcon icon3;
+        icon3.addFile(QString::fromUtf8(":/img/saveas.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionSaveAs->setIcon(icon3);
+        actionExit = new QAction(MainWindow);
+        actionExit->setObjectName(QString::fromUtf8("actionExit"));
+        QIcon icon4;
+        icon4.addFile(QString::fromUtf8(":/img/redo.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionExit->setIcon(icon4);
+        actionUndo = new QAction(MainWindow);
+        actionUndo->setObjectName(QString::fromUtf8("actionUndo"));
+        QIcon icon5;
+        icon5.addFile(QString::fromUtf8(":/img/undo.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionUndo->setIcon(icon5);
+        actionCut = new QAction(MainWindow);
+        actionCut->setObjectName(QString::fromUtf8("actionCut"));
+        QIcon icon6;
+        icon6.addFile(QString::fromUtf8(":/img/cut.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionCut->setIcon(icon6);
+        actionCopy = new QAction(MainWindow);
+        actionCopy->setObjectName(QString::fromUtf8("actionCopy"));
+        QIcon icon7;
+        icon7.addFile(QString::fromUtf8(":/img/copy.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionCopy->setIcon(icon7);
+        actionPaste = new QAction(MainWindow);
+        actionPaste->setObjectName(QString::fromUtf8("actionPaste"));
+        QIcon icon8;
+        icon8.addFile(QString::fromUtf8(":/img/paste.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionPaste->setIcon(icon8);
+        actionFind = new QAction(MainWindow);
+        actionFind->setObjectName(QString::fromUtf8("actionFind"));
+        QIcon icon9;
+        icon9.addFile(QString::fromUtf8(":/img/find.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionFind->setIcon(icon9);
+        actionReplace = new QAction(MainWindow);
+        actionReplace->setObjectName(QString::fromUtf8("actionReplace"));
+        QIcon icon10;
+        icon10.addFile(QString::fromUtf8(":/img/replace.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionReplace->setIcon(icon10);
+        actionFont = new QAction(MainWindow);
+        actionFont->setObjectName(QString::fromUtf8("actionFont"));
+        QIcon icon11;
+        icon11.addFile(QString::fromUtf8(":/img/font.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionFont->setIcon(icon11);
+        actionAutoWrap = new QAction(MainWindow);
+        actionAutoWrap->setObjectName(QString::fromUtf8("actionAutoWrap"));
+        QIcon icon12;
+        icon12.addFile(QString::fromUtf8(":/img/wrap.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionAutoWrap->setIcon(icon12);
+        actionToolBar = new QAction(MainWindow);
+        actionToolBar->setObjectName(QString::fromUtf8("actionToolBar"));
+        QIcon icon13;
+        icon13.addFile(QString::fromUtf8(":/img/tool.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionToolBar->setIcon(icon13);
+        actionStatusBar = new QAction(MainWindow);
+        actionStatusBar->setObjectName(QString::fromUtf8("actionStatusBar"));
+        QIcon icon14;
+        icon14.addFile(QString::fromUtf8(":/img/status.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionStatusBar->setIcon(icon14);
+        actionRedo = new QAction(MainWindow);
+        actionRedo->setObjectName(QString::fromUtf8("actionRedo"));
+        actionRedo->setIcon(icon4);
+        actionSelectAll = new QAction(MainWindow);
+        actionSelectAll->setObjectName(QString::fromUtf8("actionSelectAll"));
+        actionLineNumber = new QAction(MainWindow);
+        actionLineNumber->setObjectName(QString::fromUtf8("actionLineNumber"));
+        QIcon icon15;
+        icon15.addFile(QString::fromUtf8(":/img/linenumber.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionLineNumber->setIcon(icon15);
+        actionFontColor = new QAction(MainWindow);
+        actionFontColor->setObjectName(QString::fromUtf8("actionFontColor"));
+        actionFontBackgourndColor = new QAction(MainWindow);
+        actionFontBackgourndColor->setObjectName(QString::fromUtf8("actionFontBackgourndColor"));
+        actionEditorBackgroundColor = new QAction(MainWindow);
+        actionEditorBackgroundColor->setObjectName(QString::fromUtf8("actionEditorBackgroundColor"));
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName(QString::fromUtf8("actionAbout"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setSpacing(0);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(1, 1, 1, 1);
+        textEdit = new QPlainTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+
+        verticalLayout->addWidget(textEdit);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        menu = new QMenu(menubar);
+        menu->setObjectName(QString::fromUtf8("menu"));
+        menu_E = new QMenu(menubar);
+        menu_E->setObjectName(QString::fromUtf8("menu_E"));
+        menu_O = new QMenu(menubar);
+        menu_O->setObjectName(QString::fromUtf8("menu_O"));
+        menu_V = new QMenu(menubar);
+        menu_V->setObjectName(QString::fromUtf8("menu_V"));
+        menu_H = new QMenu(menubar);
+        menu_H->setObjectName(QString::fromUtf8("menu_H"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QString::fromUtf8("toolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        menubar->addAction(menu->menuAction());
+        menubar->addAction(menu_E->menuAction());
+        menubar->addAction(menu_O->menuAction());
+        menubar->addAction(menu_V->menuAction());
+        menubar->addAction(menu_H->menuAction());
+        menu->addAction(actionNew);
+        menu->addAction(actionOpen);
+        menu->addAction(actionSave);
+        menu->addAction(actionSaveAs);
+        menu->addSeparator();
+        menu->addAction(actionExit);
+        menu_E->addAction(actionUndo);
+        menu_E->addAction(actionRedo);
+        menu_E->addAction(actionCut);
+        menu_E->addAction(actionCopy);
+        menu_E->addAction(actionPaste);
+        menu_E->addSeparator();
+        menu_E->addAction(actionFind);
+        menu_E->addAction(actionReplace);
+        menu_E->addSeparator();
+        menu_E->addAction(actionSelectAll);
+        menu_O->addAction(actionAutoWrap);
+        menu_O->addAction(actionLineNumber);
+        menu_O->addAction(actionFont);
+        menu_O->addAction(actionFontColor);
+        menu_O->addSeparator();
+        menu_O->addAction(actionFontBackgourndColor);
+        menu_O->addAction(actionEditorBackgroundColor);
+        menu_V->addAction(actionToolBar);
+        menu_V->addAction(actionStatusBar);
+        menu_H->addAction(actionAbout);
+        toolBar->addAction(actionNew);
+        toolBar->addAction(actionOpen);
+        toolBar->addAction(actionSave);
+        toolBar->addAction(actionSaveAs);
+        toolBar->addSeparator();
+        toolBar->addAction(actionUndo);
+        toolBar->addAction(actionRedo);
+        toolBar->addAction(actionCut);
+        toolBar->addAction(actionCopy);
+        toolBar->addAction(actionPaste);
+        toolBar->addSeparator();
+        toolBar->addAction(actionFind);
+        toolBar->addAction(actionReplace);
+        toolBar->addSeparator();
+        toolBar->addAction(actionFont);
+        toolBar->addAction(actionLineNumber);
+        toolBar->addAction(actionAutoWrap);
+        toolBar->addSeparator();
+        toolBar->addAction(actionToolBar);
+        toolBar->addAction(actionStatusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionNew->setText(QApplication::translate("MainWindow", "\346\226\260\345\273\272(&N)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionNew->setShortcut(QApplication::translate("MainWindow", "Ctrl+N", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionOpen->setText(QApplication::translate("MainWindow", "\346\211\223\345\274\200(&O)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionOpen->setShortcut(QApplication::translate("MainWindow", "Ctrl+O", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionSave->setText(QApplication::translate("MainWindow", "\344\277\235\345\255\230(&S)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionSave->setShortcut(QApplication::translate("MainWindow", "Ctrl+S", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionSaveAs->setText(QApplication::translate("MainWindow", "\345\217\246\345\255\230\344\270\272(&S)", nullptr));
+        actionExit->setText(QApplication::translate("MainWindow", "\351\200\200\345\207\272(&X)", nullptr));
+        actionUndo->setText(QApplication::translate("MainWindow", "\346\222\244\351\224\200(&U)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionUndo->setShortcut(QApplication::translate("MainWindow", "Ctrl+Z", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionCut->setText(QApplication::translate("MainWindow", "\345\211\252\345\210\207(&T)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionCut->setShortcut(QApplication::translate("MainWindow", "Ctrl+X", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionCopy->setText(QApplication::translate("MainWindow", "\345\244\215\345\210\266(&C)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionCopy->setShortcut(QApplication::translate("MainWindow", "Ctrl+C", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionPaste->setText(QApplication::translate("MainWindow", "\347\262\230\350\264\264(&P)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionPaste->setShortcut(QApplication::translate("MainWindow", "Ctrl+V", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionFind->setText(QApplication::translate("MainWindow", "\346\237\245\346\211\276(&F)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionFind->setShortcut(QApplication::translate("MainWindow", "Ctrl+F", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionReplace->setText(QApplication::translate("MainWindow", "\346\233\277\346\215\242(&R)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionReplace->setShortcut(QApplication::translate("MainWindow", "Ctrl+H", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionFont->setText(QApplication::translate("MainWindow", "\345\255\227\344\275\223(&F)", nullptr));
+        actionAutoWrap->setText(QApplication::translate("MainWindow", "\350\207\252\345\212\250\346\215\242\350\241\214(&W)", nullptr));
+        actionToolBar->setText(QApplication::translate("MainWindow", "\345\267\245\345\205\267\346\240\217(&T)", nullptr));
+        actionStatusBar->setText(QApplication::translate("MainWindow", "\347\212\266\346\200\201\346\240\217(&S)", nullptr));
+        actionRedo->setText(QApplication::translate("MainWindow", "\346\201\242\345\244\215(&Z)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionRedo->setShortcut(QApplication::translate("MainWindow", "Ctrl+Shift+Z", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionSelectAll->setText(QApplication::translate("MainWindow", "\345\205\250\351\200\211(&A)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionSelectAll->setShortcut(QApplication::translate("MainWindow", "Ctrl+V", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionLineNumber->setText(QApplication::translate("MainWindow", "\346\230\276\347\244\272\350\241\214\345\217\267(&W)", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionLineNumber->setShortcut(QApplication::translate("MainWindow", "Ctrl+L", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionFontColor->setText(QApplication::translate("MainWindow", "\345\255\227\344\275\223\351\242\234\350\211\262", nullptr));
+        actionFontBackgourndColor->setText(QApplication::translate("MainWindow", "\345\255\227\344\275\223\350\203\214\346\231\257\350\211\262", nullptr));
+        actionEditorBackgroundColor->setText(QApplication::translate("MainWindow", "\347\274\226\350\276\221\345\231\250\350\203\214\346\231\257\350\211\262", nullptr));
+        actionAbout->setText(QApplication::translate("MainWindow", "\345\205\263\344\272\216(&A)", nullptr));
+        menu->setTitle(QApplication::translate("MainWindow", "\346\226\207\344\273\266(&F)", nullptr));
+        menu_E->setTitle(QApplication::translate("MainWindow", "\347\274\226\350\276\221(&E)", nullptr));
+        menu_O->setTitle(QApplication::translate("MainWindow", "\346\240\274\345\274\217(&O)", nullptr));
+        menu_V->setTitle(QApplication::translate("MainWindow", "\346\237\245\347\234\213(&V)", nullptr));
+        menu_H->setTitle(QApplication::translate("MainWindow", "\345\270\256\345\212\251(&H)", nullptr));
+        toolBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
