0d3d08ccd301f38e9f840bd17a3ccedad65f009f
tang <2362087759@qq.com>
2025-11-14 11:04:04 +0800

    完成Lab2剪切复制粘贴等相关操作

--- Lab2/build-Lab2-Desktop_Qt_5_12_11_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ Lab2/build-Lab2-Desktop_Qt_5_12_11_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[10];
-    char stringdata0[209];
+    QByteArrayData data[19];
+    char stringdata0[409];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -40,7 +40,16 @@ QT_MOC_LITERAL(5, 88, 22), // "on_actionNew_triggered"
 QT_MOC_LITERAL(6, 111, 23), // "on_actionOpen_triggered"
 QT_MOC_LITERAL(7, 135, 23), // "on_actionSave_triggered"
 QT_MOC_LITERAL(8, 159, 25), // "on_actionSaveAs_triggered"
-QT_MOC_LITERAL(9, 185, 23) // "on_textEdit_textChanged"
+QT_MOC_LITERAL(9, 185, 23), // "on_textEdit_textChanged"
+QT_MOC_LITERAL(10, 209, 23), // "on_actionUndo_triggered"
+QT_MOC_LITERAL(11, 233, 22), // "on_actionCut_triggered"
+QT_MOC_LITERAL(12, 256, 23), // "on_actionCopy_triggered"
+QT_MOC_LITERAL(13, 280, 24), // "on_actionPaste_triggered"
+QT_MOC_LITERAL(14, 305, 23), // "on_actionRedo_triggered"
+QT_MOC_LITERAL(15, 329, 25), // "on_textEdit_undoAvailable"
+QT_MOC_LITERAL(16, 355, 1), // "b"
+QT_MOC_LITERAL(17, 357, 25), // "on_textEdit_redoAvailable"
+QT_MOC_LITERAL(18, 383, 25) // "on_textEdit_copyAvailable"
 
     },
     "MainWindow\0on_actionAbout_triggered\0"
@@ -49,7 +58,13 @@ QT_MOC_LITERAL(9, 185, 23) // "on_textEdit_textChanged"
     "on_actionNew_triggered\0on_actionOpen_triggered\0"
     "on_actionSave_triggered\0"
     "on_actionSaveAs_triggered\0"
-    "on_textEdit_textChanged"
+    "on_textEdit_textChanged\0on_actionUndo_triggered\0"
+    "on_actionCut_triggered\0on_actionCopy_triggered\0"
+    "on_actionPaste_triggered\0"
+    "on_actionRedo_triggered\0"
+    "on_textEdit_undoAvailable\0b\0"
+    "on_textEdit_redoAvailable\0"
+    "on_textEdit_copyAvailable"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +74,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       8,   14, // methods
+      16,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,14 +82,22 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   54,    2, 0x08 /* Private */,
-       3,    0,   55,    2, 0x08 /* Private */,
-       4,    0,   56,    2, 0x08 /* Private */,
-       5,    0,   57,    2, 0x08 /* Private */,
-       6,    0,   58,    2, 0x08 /* Private */,
-       7,    0,   59,    2, 0x08 /* Private */,
-       8,    0,   60,    2, 0x08 /* Private */,
-       9,    0,   61,    2, 0x08 /* Private */,
+       1,    0,   94,    2, 0x08 /* Private */,
+       3,    0,   95,    2, 0x08 /* Private */,
+       4,    0,   96,    2, 0x08 /* Private */,
+       5,    0,   97,    2, 0x08 /* Private */,
+       6,    0,   98,    2, 0x08 /* Private */,
+       7,    0,   99,    2, 0x08 /* Private */,
+       8,    0,  100,    2, 0x08 /* Private */,
+       9,    0,  101,    2, 0x08 /* Private */,
+      10,    0,  102,    2, 0x08 /* Private */,
+      11,    0,  103,    2, 0x08 /* Private */,
+      12,    0,  104,    2, 0x08 /* Private */,
+      13,    0,  105,    2, 0x08 /* Private */,
+      14,    0,  106,    2, 0x08 /* Private */,
+      15,    1,  107,    2, 0x08 /* Private */,
+      17,    1,  110,    2, 0x08 /* Private */,
+      18,    1,  113,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -85,6 +108,14 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   16,
+    QMetaType::Void, QMetaType::Bool,   16,
+    QMetaType::Void, QMetaType::Bool,   16,
 
        0        // eod
 };
@@ -103,10 +134,17 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 5: _t->on_actionSave_triggered(); break;
         case 6: _t->on_actionSaveAs_triggered(); break;
         case 7: _t->on_textEdit_textChanged(); break;
+        case 8: _t->on_actionUndo_triggered(); break;
+        case 9: _t->on_actionCut_triggered(); break;
+        case 10: _t->on_actionCopy_triggered(); break;
+        case 11: _t->on_actionPaste_triggered(); break;
+        case 12: _t->on_actionRedo_triggered(); break;
+        case 13: _t->on_textEdit_undoAvailable((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 14: _t->on_textEdit_redoAvailable((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 15: _t->on_textEdit_copyAvailable((*reinterpret_cast< bool(*)>(_a[1]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
@@ -138,13 +176,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 8)
+        if (_id < 16)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 8;
+        _id -= 16;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 8)
+        if (_id < 16)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 8;
+        _id -= 16;
     }
     return _id;
 }
